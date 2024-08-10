75b7ce53368a20aae3ca0a5b5d96afd7db62c2ff
Sergey Alexeev <sergeyalexeev@yahoo.com>
2024-08-10 14:43:00 +0300

    check

--- runtime/split.go
+++ runtime/split.go
@@ -101,7 +101,7 @@ func MakeSplitStream[T any](name string, stream TypedStream[T], count int) *Spli
 }
 
 func (s *SplitStream[T]) Get(index int) TypedStream[T] {
-	if index < 0 || len(s.links) >= index {
+	if index < 0 || index >= len(s.links) {
 		log.Panicf("Get index=%d for the split stream %d is invaid", index, s.config.Id)
 	}
 	return s.links[index]
