e652fe5177b71cfe349a826fdf05ec4f445c941b
Sergey Alexeev <sergeyalexeev@yahoo.com>
2024-08-25 12:42:17 +0300

    remove code redundancy

--- runtime/split.go
+++ runtime/split.go
@@ -8,193 +8,191 @@
 package runtime
 
 import (
-    log "github.com/sirupsen/logrus"
-    "strconv"
+	log "github.com/sirupsen/logrus"
+	"strconv"
 )
 
 type SplitLink[T any] struct {
-    splitStream *SplitStream[T]
-    index       int
-    caller      Caller[T]
-    consumer    TypedStreamConsumer[T]
+	splitStream *SplitStream[T]
+	index       int
+	caller      Caller[T]
+	consumer    TypedStreamConsumer[T]
 }
 
 func (s *SplitLink[T]) GetId() int {
-    return s.splitStream.GetId()
+	return s.splitStream.GetId()
 }
 
 func (s *SplitLink[T]) GetName() string {
-    return s.splitStream.GetName() + "SplitLink" + strconv.Itoa(s.index)
+	return s.splitStream.GetName() + "SplitLink" + strconv.Itoa(s.index)
 }
 
 func (s *SplitLink[T]) GetRuntime() StreamExecutionRuntime {
-    return s.splitStream.GetRuntime()
+	return s.splitStream.GetRuntime()
 }
 
 func (s *SplitLink[T]) GetConfig() *StreamConfig {
-    return s.splitStream.GetConfig()
+	return s.splitStream.GetConfig()
 }
 
 func (s *SplitLink[T]) setConsumer(consumer TypedStreamConsumer[T]) {
-    s.consumer = consumer
-    s.caller = makeCaller[T](s.splitStream.runtime, s)
+	s.consumer = consumer
+	s.caller = makeCaller[T](s.splitStream.runtime, s)
 }
 
 func (s *SplitLink[T]) GetTransformationName() string {
-    return s.splitStream.GetTransformationName()
+	return s.splitStream.GetTransformationName()
 }
 
 func (s *SplitLink[T]) GetTypeName() string {
-    return s.splitStream.GetTypeName()
+	return s.splitStream.GetTypeName()
 }
 
 func (s *SplitLink[T]) GetSerde() StreamSerde[T] {
-    return s.splitStream.GetSerde()
+	return s.splitStream.GetSerde()
 }
 
 func (s *SplitLink[T]) getConsumers() []StreamBase {
-    return s.splitStream.getConsumers()
+	return s.splitStream.getConsumers()
 }
 
 func (s *SplitLink[T]) Consume(value T) {
-    if s.caller != nil {
-        s.caller.Consume(value)
-    }
+	if s.caller != nil {
+		s.caller.Consume(value)
+	}
 }
 
 func (s *SplitLink[T]) GetConsumer() TypedStreamConsumer[T] {
-    return s.consumer
+	return s.consumer
 }
 
 func splitLink[T any](index int, splitStream *SplitStream[T]) *SplitLink[T] {
-    link := SplitLink[T]{
-        splitStream: splitStream,
-        index:       index,
-    }
-    return &link
+	link := SplitLink[T]{
+		splitStream: splitStream,
+		index:       index,
+	}
+	return &link
 }
 
 type SplitStream[T any] struct {
-    *ConsumedStream[T]
-    links  []*SplitLink[T]
-    source TypedStream[T]
+	*ConsumedStream[T]
+	links  []*SplitLink[T]
+	source TypedStream[T]
 }
 
 type InputSplitStream[T any] struct {
-    *SplitStream[T]
+	*SplitStream[T]
 }
 
 type InputKVSplitStream[T any] struct {
-    *SplitStream[T]
-    serdeKV StreamKeyValueSerde[T]
+	*SplitStream[T]
+	serdeKV StreamKeyValueSerde[T]
 }
 
 func (s *InputSplitStream[T]) ConsumeBinary(data []byte) {
-    t, err := s.serde.Deserialize(data)
-    if err != nil {
-        log.Errorln(err)
-    } else {
-        s.caller.Consume(t)
-    }
+	t, err := s.serde.Deserialize(data)
+	if err != nil {
+		log.Errorln(err)
+	} else {
+		s.caller.Consume(t)
+	}
 }
 
 func (s *InputKVSplitStream[T]) ConsumeBinary(key []byte, value []byte) {
-    t, err := s.serdeKV.DeserializeKeyValue(key, value)
-    if err != nil {
-        log.Errorln(err)
-    } else {
-        s.caller.Consume(t)
-    }
+	t, err := s.serdeKV.DeserializeKeyValue(key, value)
+	if err != nil {
+		log.Errorln(err)
+	} else {
+		s.caller.Consume(t)
+	}
 }
 
 func MakeSplitStream[T any](name string, stream TypedStream[T]) *SplitStream[T] {
-    runtime := stream.GetRuntime()
-    config := runtime.GetConfig()
-    streamConfig := config.GetStreamConfigByName(name)
-    if streamConfig == nil {
-        log.Fatalf("Config for the stream with name=%s does not exists", name)
-    }
-    splitStream := &SplitStream[T]{
-        ConsumedStream: &ConsumedStream[T]{
-            Stream: &Stream[T]{
-                runtime: runtime,
-                config:  *streamConfig,
-            },
-            serde: stream.GetSerde(),
-        },
-        source: stream,
-        links:  make([]*SplitLink[T], 0),
-    }
-    runtime.registerStream(splitStream)
-    stream.setConsumer(splitStream)
-    return splitStream
+	runtime := stream.GetRuntime()
+	config := runtime.GetConfig()
+	streamConfig := config.GetStreamConfigByName(name)
+	if streamConfig == nil {
+		log.Fatalf("Config for the stream with name=%s does not exists", name)
+	}
+	splitStream := &SplitStream[T]{
+		ConsumedStream: &ConsumedStream[T]{
+			Stream: &Stream[T]{
+				runtime: runtime,
+				config:  *streamConfig,
+			},
+			serde: stream.GetSerde(),
+		},
+		source: stream,
+		links:  make([]*SplitLink[T], 0, 2),
+	}
+	runtime.registerStream(splitStream)
+	stream.setConsumer(splitStream)
+	return splitStream
 }
 
 func MakeInputSplitStream[T any](name string, runtime StreamExecutionRuntime) *InputSplitStream[T] {
-    config := runtime.GetConfig()
-    streamConfig := config.GetStreamConfigByName(name)
-    if streamConfig == nil {
-        log.Fatalf("Config for the stream with name=%s does not exists", name)
-    }
-    inputSplitStream := &InputSplitStream[T]{
-        SplitStream: &SplitStream[T]{
-            ConsumedStream: &ConsumedStream[T]{
-                Stream: &Stream[T]{
-                    runtime: runtime,
-                    config:  *streamConfig,
-                },
-                serde: makeSerde[T](runtime),
-            },
-            links: make([]*SplitLink[T], 0),
-        },
-    }
-    runtime.registerStream(inputSplitStream)
-    return inputSplitStream
+	config := runtime.GetConfig()
+	streamConfig := config.GetStreamConfigByName(name)
+	if streamConfig == nil {
+		log.Fatalf("Config for the stream with name=%s does not exists", name)
+	}
+	inputSplitStream := &InputSplitStream[T]{
+		SplitStream: &SplitStream[T]{
+			ConsumedStream: &ConsumedStream[T]{
+				Stream: &Stream[T]{
+					runtime: runtime,
+					config:  *streamConfig,
+				},
+				serde: makeSerde[T](runtime),
+			},
+			links: make([]*SplitLink[T], 0, 1),
+		},
+	}
+	runtime.registerStream(inputSplitStream)
+	return inputSplitStream
 }
 
 func MakeInputKVSplitStream[T any](name string, runtime StreamExecutionRuntime) *InputKVSplitStream[T] {
-    config := runtime.GetConfig()
-    streamConfig := config.GetStreamConfigByName(name)
-    if streamConfig == nil {
-        log.Fatalf("Config for the stream with name=%s does not exists", name)
-    }
-    serdeKV := makeSerde[T](runtime).(StreamKeyValueSerde[T])
-    inputKVSplitStream := &InputKVSplitStream[T]{
-        SplitStream: &SplitStream[T]{
-            ConsumedStream: &ConsumedStream[T]{
-                Stream: &Stream[T]{
-                    runtime: runtime,
-                    config:  *streamConfig,
-                },
-                serde: serdeKV,
-            },
-            links: make([]*SplitLink[T], 0),
-        },
-        serdeKV: serdeKV,
-    }
-    runtime.registerStream(inputKVSplitStream)
-    return inputKVSplitStream
+	config := runtime.GetConfig()
+	streamConfig := config.GetStreamConfigByName(name)
+	if streamConfig == nil {
+		log.Fatalf("Config for the stream with name=%s does not exists", name)
+	}
+	serdeKV := makeSerde[T](runtime).(StreamKeyValueSerde[T])
+	inputKVSplitStream := &InputKVSplitStream[T]{
+		SplitStream: &SplitStream[T]{
+			ConsumedStream: &ConsumedStream[T]{
+				Stream: &Stream[T]{
+					runtime: runtime,
+					config:  *streamConfig,
+				},
+				serde: serdeKV,
+			},
+			links: make([]*SplitLink[T], 0),
+		},
+		serdeKV: serdeKV,
+	}
+	runtime.registerStream(inputKVSplitStream)
+	return inputKVSplitStream
 }
 
 func (s *SplitStream[T]) AddStream() TypedConsumedStream[T] {
-    index := len(s.links)
-    link := splitLink[T](index, s)
-    s.links = append(s.links, link)
-    return s.links[index]
+	index := len(s.links)
+	link := splitLink[T](index, s)
+	s.links = append(s.links, link)
+	return s.links[index]
 }
 
 func (s *SplitStream[T]) Consume(value T) {
-    for i := 0; i < len(s.links); i++ {
-        if s.links[i] != nil {
-            s.links[i].Consume(value)
-        }
-    }
+	for i := 0; i < len(s.links); i++ {
+		s.links[i].Consume(value)
+	}
 }
 
 func (s *SplitStream[T]) getConsumers() []StreamBase {
-    var consumers = make([]StreamBase, len(s.links))
-    for i := 0; i < len(s.links); i++ {
-        consumers[i] = s.links[i].GetConsumer()
-    }
-    return consumers
+	var consumers = make([]StreamBase, len(s.links))
+	for i := 0; i < len(s.links); i++ {
+		consumers[i] = s.links[i].GetConsumer()
+	}
+	return consumers
 }
