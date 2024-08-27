712101bcddb1bbf0978f9ec98a391dec35021368
Sergey Alexeev <sergeyalexeev@yahoo.com>
2024-08-27 23:59:50 +0300

    store changes

--- runtime/store/hashmap.go
+++ runtime/store/hashmap.go
@@ -13,18 +13,14 @@ import (
 	"time"
 )
 
-const clearInterval = time.Duration(1) * time.Second
-
-type Item[K comparable] struct {
+type Item struct {
 	values    [][]interface{}
-	key       K
 	lock      sync.Mutex
 	deadline  time.Time
-	next      *Item[K]
 	processed atomic.Bool
 }
 
-func (item *Item[K]) SetValue(index int, value interface{}, f JoinValueFunc) bool {
+func (item *Item) SetValue(index int, value interface{}, f JoinValueFunc) bool {
 	item.lock.Lock()
 	defer item.lock.Unlock()
 	if len(item.values) <= index {
@@ -39,164 +35,41 @@ func (item *Item[K]) SetValue(index int, value interface{}, f JoinValueFunc) boo
 }
 
 type HashMapJoinStorage[K comparable] struct {
-	storage           map[K]*Item[K]
-	ttl               time.Duration
-	lock              sync.RWMutex
-	deadlineListFirst *Item[K]
-	deadlineListLast  *Item[K]
-	timer             *time.Timer
-}
-
-func (s *HashMapJoinStorage[K]) processDeadline() {
-	deadlineReached := func() *Item[K] {
-		var deadlineReached *Item[K]
-		var deadlineReachedLast *Item[K]
-		s.lock.Lock()
-		defer s.lock.Unlock()
-		if s.deadlineListFirst == nil {
-			return deadlineReached
-		}
-		it := s.deadlineListFirst
-		s.deadlineListFirst = nil
-		s.deadlineListLast = nil
-		before := false
-		now := time.Now()
-		for it != nil {
-			before = before || now.Before(it.deadline)
-			processed := it.processed.Load()
-			if !before {
-				if !processed {
-					if deadlineReached == nil {
-						deadlineReached = it
-						deadlineReachedLast = it
-					} else {
-						deadlineReachedLast.next = it
-						deadlineReachedLast = it
-					}
-					it = it.next
-					deadlineReachedLast.next = nil
-				} else {
-					it = it.next
-				}
-			} else {
-				if !processed {
-					if s.deadlineListFirst == nil {
-						s.deadlineListFirst = it
-						s.deadlineListLast = it
-					} else {
-						s.deadlineListLast.next = it
-						s.deadlineListLast = it
-					}
-					it = it.next
-					s.deadlineListLast.next = nil
-				} else {
-					it = it.next
-				}
-			}
-		}
-		if s.deadlineListFirst != nil {
-			s.timer.Reset(clearInterval)
-		}
-		return deadlineReached
-	}()
-	for ; deadlineReached != nil; deadlineReached = deadlineReached.next {
-		func(item *Item[K]) {
-			if item.processed.CompareAndSwap(false, true) {
-				s.lock.Lock()
-				defer s.lock.Unlock()
-				delete(s.storage, item.key)
-			}
-		}(deadlineReached)
-	}
-}
-
-func (s *HashMapJoinStorage[K]) JoinValue(key K, index int, value interface{}, f JoinValueFunc) {
-	item := func() *Item[K] {
-		s.lock.RLock()
-		item := s.storage[key]
-		s.lock.RUnlock()
-		if item != nil && time.Now().Before(item.deadline) {
-			return item
-		}
-		newItem := &Item[K]{
-			values: make([][]interface{}, index+1, 2),
-		}
-		s.lock.Lock()
-		defer s.lock.Unlock()
-		if item == nil {
-			item = s.storage[key]
-			if item != nil && time.Now().Before(item.deadline) {
-				return item
-			}
-		}
-		if item != nil {
-			item.processed.Store(true)
-		}
-		s.storage[key] = newItem
-		if s.ttl > 0 {
-			newItem.deadline = time.Now().Add(s.ttl)
-			if s.deadlineListLast == nil {
-				s.deadlineListLast = newItem
-				s.deadlineListFirst = newItem
-				if s.timer == nil {
-					s.timer = time.AfterFunc(clearInterval, s.processDeadline)
-				} else {
-					s.timer.Reset(clearInterval)
-				}
-			} else {
-				s.deadlineListLast.next = newItem
-				s.deadlineListLast = newItem
-			}
-		}
-		return newItem
-	}()
-	if item.SetValue(index, value, f) {
-		s.lock.Lock()
-		defer s.lock.Unlock()
-		delete(s.storage, key)
-	}
-}
-
-func MakeHashMapJoinStorage[K comparable](ttl time.Duration) JoinStorage[K] {
-	return &HashMapJoinStorage[K]{
-		storage: make(map[K]*Item[K]),
-		ttl:     ttl,
-	}
-}
-
-type RotateHashMapJoinStorage[K comparable] struct {
-	storage1   map[K]*Item[K]
-	storage2   map[K]*Item[K]
+	storage1   map[K]*Item
+	storage2   map[K]*Item
 	ttl        time.Duration
 	rotateLock sync.RWMutex
 	lock       sync.RWMutex
 	timer      *time.Timer
 }
 
-func MakeRotateHashMapJoinStorage[K comparable](ttl time.Duration) JoinStorage[K] {
-	return &RotateHashMapJoinStorage[K]{
-		storage1: make(map[K]*Item[K]),
-		storage2: make(map[K]*Item[K]),
+func MakeHashMapJoinStorage[K comparable](ttl time.Duration) JoinStorage[K] {
+	joinStorage := &HashMapJoinStorage[K]{
+		storage1: make(map[K]*Item),
 		ttl:      ttl,
 	}
+	if ttl > 0 {
+		joinStorage.storage2 = make(map[K]*Item)
+	}
+	return joinStorage
 }
 
-func (s *RotateHashMapJoinStorage[K]) rotate() {
+func (s *HashMapJoinStorage[K]) rotate() {
 	s.rotateLock.Lock()
 	defer s.rotateLock.Unlock()
 	s.storage2 = s.storage1
-	s.storage1 = make(map[K]*Item[K])
+	s.storage1 = make(map[K]*Item)
 	s.timer.Reset(s.ttl)
 }
 
-func (s *RotateHashMapJoinStorage[K]) JoinValue(key K, index int, value interface{}, f JoinValueFunc) {
-	s.rotateLock.RLock()
-	defer s.rotateLock.RUnlock()
+func (s *HashMapJoinStorage[K]) JoinValue(key K, index int, value interface{}, f JoinValueFunc) {
 	if s.timer == nil && s.ttl > 0 {
+		s.rotateLock.RLock()
+		defer s.rotateLock.RUnlock()
 		s.timer = time.AfterFunc(s.ttl, s.rotate)
 	}
-	item, storage := func() (*Item[K], map[K]*Item[K]) {
-		item, storage := func() (*Item[K], map[K]*Item[K]) {
+	item, storage := func() (*Item, map[K]*Item) {
+		item, storage := func() (*Item, map[K]*Item) {
 			s.lock.RLock()
 			defer s.lock.RUnlock()
 			if s.ttl > 0 {
@@ -222,8 +95,11 @@ func (s *RotateHashMapJoinStorage[K]) JoinValue(key K, index int, value interfac
 			} else if &storage != &s.storage1 {
 				delete(storage, key)
 			}
-			newItem := &Item[K]{
+			newItem := &Item{
 				values: make([][]interface{}, index+1, 2),
+			}
+			if s.ttl > 0 {
+				newItem.deadline = time.Now().Add(s.ttl)
 			}
 			s.storage1[key] = newItem
 			return item, s.storage1
