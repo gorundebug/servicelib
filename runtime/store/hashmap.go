3e0afdb086e87664d0d1900413f2e91e4bfaf2a5
Sergey Alexeev <sergeyalexeev@yahoo.com>
2024-08-28 01:08:40 +0300

    store changes

--- runtime/store/hashmap.go
+++ runtime/store/hashmap.go
@@ -9,7 +9,6 @@ package store
 
 import (
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -17,21 +16,7 @@ type Item struct {
 	values    [][]interface{}
 	lock      sync.Mutex
 	deadline  time.Time
-	processed atomic.Bool
-}
-
-func (item *Item) SetValue(index int, value interface{}, f JoinValueFunc) bool {
-	item.lock.Lock()
-	defer item.lock.Unlock()
-	if len(item.values) <= index {
-		item.values = append(item.values, make([][]interface{}, index-len(item.values)+1)...)
-	}
-	item.values[index] = append(item.values[index], value)
-	if f(item.values) {
-		item.processed.Store(true)
-		return true
-	}
-	return false
+	processed bool
 }
 
 type HashMapJoinStorage[K comparable] struct {
@@ -68,47 +53,76 @@ func (s *HashMapJoinStorage[K]) JoinValue(key K, index int, value interface{}, f
 		defer s.rotateLock.RUnlock()
 		s.timer = time.AfterFunc(s.ttl, s.rotate)
 	}
-	item, storage := func() (*Item, map[K]*Item) {
+
+	for {
 		item, storage := func() (*Item, map[K]*Item) {
-			s.lock.RLock()
-			defer s.lock.RUnlock()
-			if s.ttl > 0 {
-				item := s.storage2[key]
-				if item != nil {
-					return item, s.storage2
+
+			item, storage := func() (*Item, map[K]*Item) {
+				s.lock.RLock()
+				defer s.lock.RUnlock()
+				if s.ttl > 0 {
+					item := s.storage2[key]
+					if item != nil {
+						return item, s.storage2
+					}
 				}
-			}
-			item := s.storage1[key]
-			if item != nil {
-				return item, s.storage1
-			}
-			return nil, nil
-		}()
-		if item == nil || time.Now().Before(item.deadline) {
-			s.lock.Lock()
-			defer s.lock.Unlock()
-			if item == nil {
-				item = s.storage1[key]
-				if item != nil && !time.Now().Before(item.deadline) {
+				item := s.storage1[key]
+				if item != nil {
 					return item, s.storage1
 				}
-			} else if &storage != &s.storage1 {
-				delete(storage, key)
+				return nil, nil
+			}()
+
+			if item == nil || time.Now().Before(item.deadline) {
+				newItem := &Item{
+					values: make([][]interface{}, index+1, 2),
+				}
+				if s.ttl > 0 {
+					newItem.deadline = time.Now().Add(s.ttl)
+				}
+				s.lock.Lock()
+				defer s.lock.Unlock()
+				if item == nil {
+					item = s.storage1[key]
+					if item != nil && !time.Now().Before(item.deadline) {
+						return item, s.storage1
+					}
+				} else if &storage != &s.storage1 {
+					delete(storage, key)
+				}
+				s.storage1[key] = newItem
+				return item, s.storage1
 			}
-			newItem := &Item{
-				values: make([][]interface{}, index+1, 2),
+			return item, storage
+		}()
+
+		if func(item *Item) bool {
+			item.lock.Lock()
+			defer item.lock.Unlock()
+			if !item.processed {
+				if len(item.values) <= index {
+					item.values = append(item.values, make([][]interface{}, index-len(item.values)+1)...)
+				}
+				item.values[index] = append(item.values[index], value)
+				item.processed = f(item.values)
+				if item.processed {
+					s.lock.Lock()
+					defer s.lock.Unlock()
+					delete(storage, key)
+				} // else { //Depend on logic: should we extend deadline after change or not
+				//  s.lock.Lock()
+				//  defer s.lock.Unlock()
+				//  if &storage == &s.storage1 {
+				//      delete(storage, key)
+				//  }
+				//  item.deadline = time.Now().Add(s.ttl)
+				//  s.storage1[key] = item
+				// }
+				return true
 			}
-			if s.ttl > 0 {
-				newItem.deadline = time.Now().Add(s.ttl)
-			}
-			s.storage1[key] = newItem
-			return item, s.storage1
+			return false
+		}(item) {
+			break
 		}
-		return item, storage
-	}()
-	if item.SetValue(index, value, f) {
-		s.lock.Lock()
-		defer s.lock.Unlock()
-		delete(storage, key)
 	}
 }
