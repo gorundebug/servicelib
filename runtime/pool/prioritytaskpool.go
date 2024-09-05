e8f3cf20c939f869596f72a8e09075ee2aebc054
Sergey Alexeev <sergeyalexeev@yahoo.com>
2024-09-05 23:33:00 +0300

    pool package

--- runtime/pool/prioritytaskpool.go
+++ runtime/pool/prioritytaskpool.go
@@ -10,9 +10,41 @@ package pool
 type PriorityTask struct {
 	fn       func()
 	priority int
+	index    int
 }
 
 type PriorityTaskPool interface {
 	Pool
-	Execute(fn func()) *PriorityTask
+	Execute(fn func(), priority int) *PriorityTask
+}
+
+type TaskPriorityQueue []*PriorityTask
+
+func (pq *TaskPriorityQueue) Len() int { return len(*pq) }
+
+func (pq *TaskPriorityQueue) Less(i, j int) bool {
+	return (*pq)[i].priority < (*pq)[j].priority
+}
+
+func (pq *TaskPriorityQueue) Swap(i, j int) {
+	(*pq)[i], (*pq)[j] = (*pq)[j], (*pq)[i]
+	(*pq)[i].index = i
+	(*pq)[j].index = j
+}
+
+func (pq *TaskPriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*PriorityTask)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *TaskPriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
 }
--- runtime/pool/taskpool.go
+++ runtime/pool/taskpool.go
@@ -7,11 +7,36 @@
 
 package pool
 
+import "sync"
+
 type Task struct {
-	fn func()
+	fn   func()
+	next *Task
+	prev *Task
 }
 
 type TaskPool interface {
 	Pool
-	Execute(fn func()) *Task
+	AddTask(fn func()) *Task
+}
+
+type TaskPoolImpl struct {
+	head *Task
+	tail *Task
+	lock sync.Mutex
+	name string
+}
+
+func (p *TaskPoolImpl) AddTask(fn func()) *Task {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	task := &Task{fn: fn}
+	if p.tail != nil {
+		task.prev = p.tail
+		p.tail.next = task
+	} else {
+		p.head = task
+	}
+	p.tail = task
+	return task
 }
