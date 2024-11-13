6ad76e9eddbb23b75b032c8cb0b48bc9cad425b2
Sergey Alexeev <sergeyalexeev@yahoo.com>
2024-11-13 17:01:57 +0300

    diagrams

--- build_diagram.sh
+++ build_diagram.sh
@@ -4,8 +4,9 @@ path=$(dirname "$0")
 cd "$path" || { echo "Error: Failed to change directory to script location"; exit 1; }
 
 #go install github.com/davidschlachter/embedded-struct-visualizer@latest
-rm -f diagram.dot
-embedded-struct-visualizer -out diagram.dot
-dot -Tpng diagram.dot -o diagram.png
+rm -f ./diagram/diagram.dot
+rm -f ./diagram/diagram.png
+embedded-struct-visualizer -out ./diagram/diagram.dot
+dot -Tpng ./diagramdiagram.dot -o ./diagram/diagram.png
 
 cd "$original_dir" || { echo "Error: Failed to change directory to '$original_dir'"; exit 1; }
