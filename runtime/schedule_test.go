1d46b56a1f6711b1e90eb78f6412a212cf7b0b56
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-24 00:45:40 +0300

    Fix shared schedule trigger fixture

--- runtime/schedule_test.go
+++ runtime/schedule_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestScheduleTriggerIdentityIsStableAcrossRetryAndTimezone(t *testing.T) {
-	scheduledUTC := time.Date(2026, 8, 24, 12, 30, 0, 123, time.UTC)
+	scheduledUTC := time.Date(2026, 8, 24, 12, 30, 0, 123456000, time.UTC)
 	firedAt := scheduledUTC.Add(2 * time.Second)
 	first := NewScheduleTrigger(17, "hourly", scheduledUTC, firedAt, ScheduleBackendTemporal)
 	retry := NewScheduleTrigger(17, "hourly", scheduledUTC.In(time.FixedZone("offset", 3*60*60)), firedAt.Add(time.Second), ScheduleBackendTemporal)
@@ -17,6 +17,7 @@ func TestScheduleTriggerIdentityIsStableAcrossRetryAndTimezone(t *testing.T) {
 	require.Equal(t, scheduledUTC, first.ScheduledAt)
 	require.Equal(t, ScheduleBackendTemporal, first.Backend)
 	require.Len(t, first.TriggerID, 64)
+	require.Equal(t, "29b272e3eeee0c67fe5b5a121f8f39d4b5d9625d656e8a0ec7f2b0f1615e2914", first.TriggerID)
 }
 
 func TestNormalizeTemporalPriority(t *testing.T) {
