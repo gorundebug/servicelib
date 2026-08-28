949f51b36990396e555f2f17f094e407faa5f07f
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-28 05:14:48 +0300

    Route dashboard tooling through dependency proxy

--- grafana/generate.sh
+++ grafana/generate.sh
@@ -9,9 +9,14 @@ OUT_DIR="$SCRIPT_DIR/dist"
 cd "$SCRIPT_DIR"
 
 echo "==> Building Docker image for ${SERVICE_NAME}..."
+read -r -a dependency_docker_args <<< "${DEPENDENCY_PROXY_DOCKER_ARGS:-}"
 docker build \
+    "${dependency_docker_args[@]}" \
     --build-arg SERVICE_NAME="${SERVICE_NAME}" \
+    --build-arg DEPENDENCY_DOCKER_REGISTRY="${DEPENDENCY_DOCKER_REGISTRY:-docker.io}" \
     --build-arg DEPENDENCY_GITHUB_RAW_URL="${DEPENDENCY_GITHUB_RAW_URL:-https://github.com}" \
+    --build-arg DEPENDENCY_APT_DEBIAN_URL="${DEPENDENCY_APT_DEBIAN_URL:-}" \
+    --build-arg DEPENDENCY_APT_DEBIAN_SECURITY_URL="${DEPENDENCY_APT_DEBIAN_SECURITY_URL:-}" \
     -t "$IMAGE" .
 
 mkdir -p "$OUT_DIR"
